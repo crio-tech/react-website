cd6520d4616b6f992934255fd5340070cc7a5c0e
Mohammed Haris <mohammedharis.k.01@gmail.com>
2020-08-31 23:49:09 +0530

    responsive navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,91 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./NavBar.css";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { MdFingerprint } from 'react-icons/md';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { Button } from './Button';
+import './NavBar.css';
 
-function navBar() {
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
-      <Navbar bg="dark" variant="dark" className='nav-bar'>
-        <Navbar.Brand>
-          <Link to="/" className="navbar-logo">
-            Gowtham
-          </Link>
-        </Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link>
-            <Link to="/about">About</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/services">Services</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/contact">Contact</Link>
-          </Nav.Link>
-        </Nav>
-      </Navbar>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <nav className='navbar'>
+          <div className='navbar-container container'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              <MdFingerprint className='navbar-icon' />
+              Gowtham
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/services'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link
+                  to='/about'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
+                  About
+                </Link>
+              </li>
+              <li className='nav-btn'>
+                {button ? (
+                  <Link to='/contact' className='btn-link'>
+                    <Button buttonStyle='btn--outline'>Contact</Button>
+                  </Link>
+                ) : (
+                  <Link to='/contact' className='btn-link'>
+                    <Button
+                      buttonStyle='btn--outline'
+                      buttonSize='btn--mobile'
+                      onClick={closeMobileMenu}
+                    >
+                      Contact
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </IconContext.Provider>
     </>
   );
 }
 
-export default navBar;
+export default Navbar;
